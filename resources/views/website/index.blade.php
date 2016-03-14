a2067ebbac282cf0761b3f97df15bb0934244fd8
vagrant <vagrant@homestead>
2016-03-14 01:15:30 +0000

    Primera Version

---
+++ resources/views/website/index.blade.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Líbreria</title>
+	<link rel="stylesheet" href="/css/materialize.min.css">
+	<link rel="stylesheet" href="/css/app.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
+	<nav class="#00838f cyan darken-3">
+	    <div class="nav-wrapper">
+	      <a href="#" class="brand-logo" ><i class="fa fa-book" ></i> Librería</a>
+	      <a href="#" data-activates="mobile-demo" class="button-collapse" style=" left: 24px;" ><i class="fa fa-bars"></i></a>
+	      <ul id="nav-mobile" class="right hide-on-med-and-down">
+	        <li><a href="/tienda"><i class="fa fa-shopping-bag"></i> Tienda</a></li>
+	        <li><a href=""><i class="fa fa-shopping-cart"></i> Carrito</a></li>
+			<li><a href=""><i class="fa fa-question"></i> Acerca de</a></li>
+			<li><a href=""><i class="fa fa-phone"></i> Contacto</a></li>
+			<li><a href=""><i class="fa fa-sign-in"></i> Acceder</a></li>
+	      </ul>
+	      <ul id="mobile-demo" class="side-nav">
+			<li><a href=""><i class="fa fa-shopping-bag"></i> Tienda</a></li>
+	        <li><a href=""><i class="fa fa-shopping-cart"></i> Carrito</a></li>
+			<li><a href=""><i class="fa fa-question"></i> Acerca de</a></li>
+			<li><a href=""><i class="fa fa-phone"></i> Contacto</a></li>
+			<li><a href=""><i class="fa fa-sign-in"></i> Acceder</a></li>
+	      </ul>
+		</div>
+	</nav>
+
+	<section id="slider-libreria" class="container">
+		 <div class="slider">
+		    <ul class="slides">
+		      <li>
+		        <img src="http://elcinco.mx/wp-content/uploads/2015/09/libros10.jpg"> <!-- random image -->
+		        <div class="caption center-align">
+		          <h3>This is our big Tagline!</h3>
+		          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+		        </div>
+		      </li>
+		      <li>
+		        <img src="https://carmelourso.files.wordpress.com/2010/01/lector-o-lectora.jpg"> <!-- random image -->
+		        <div class="caption left-align">
+		          <h3>Left Aligned Caption</h3>
+		          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+		        </div>
+		      </li>
+		      <li>
+		        <img src="http://www.vivelohoy.com/wp-content/uploads/2012/06/lectura.jpg.jpg"> <!-- random image -->
+		        <div class="caption right-align">
+		          <h3>Right Aligned Caption</h3>
+		          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+		        </div>
+		      </li>
+		      <li>
+		        <img src="http://static.imujer.com/sites/default/files/c/como-organizar-un-librero-1.jpg"> <!-- random image -->
+		        <div class="caption center-align">
+		          <h3>This is our big Tagline!</h3>
+		          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+		        </div>
+		      </li>
+		    </ul>
+		 </div>
+	</section>
+
+	<section id="section2" class="container">
+		<div class="row">
+			<div class="col s12 l4">
+				<center>
+					<i class="fa fa-check fa-4x"></i>
+					<h5>Calidad</h5>
+					<p>Nos preocupamos por tí, por lo cual
+					puedes estar seguro de que nuestros productos 
+					y servicios son de excelente calidad.</p>
+				</center>
+			</div>
+
+			<div class="col s12 l4">
+				<center>
+					<i class="fa fa-truck fa-4x"></i>
+					<h5>Envíos</h5>
+					<p>Envíos a todos los estados de la
+					República Mexicana.</p>
+				</center>
+			</div>
+
+			<div class="col s12 l4">
+				<center>
+					<i class="fa fa-credit-card fa-4x"></i>
+					<h5>Pago Fácil</h5>
+					<p>Los pagos se pueden realizar a través
+					de un depósito en el banco o por medio de
+					una tarjeta de crédito.</p>
+				</center>
+			</div>
+		</div>
+	</section>
+      
+        <footer class="page-footer cyan darken-3 ">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text"><i class="fa fa-book" ></i> Librería</h5>
+                <p class="grey-text text-lighten-4">Para viajar lejos, no hay mejor nave que un LIBRO</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Enlaces</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Tienda</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Carrito</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Acerca de</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Contacto</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Contacto</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+	            © 2016 Guadalupe Lizek Mejía López
+	            <a class="grey-text text-lighten-4 right btn-social" href="#!"><i class="fa fa-google"></i></a>
+	            <a class="grey-text text-lighten-4 right btn-social" href="#!"><i class="fa fa-twitter-square"></i></a>
+	            <a class="grey-text text-lighten-4 right btn-social" href="#!"><i class="fa fa-facebook-square"></i></a>
+            </div>
+          </div>
+        </footer>
+            
+
+
+	<script src="/js/jquery-2.2.1.min.js"></script>
+    <script src="/js/materialize.min.js"></script>
+    <script src="/js/app.js"></script>
+</body>
+</html>
